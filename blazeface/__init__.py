82710ddb86664a4dd24c7f07c5914aed76492584
tkat0 <tkato.dev@gmail.com>
2019-06-21 10:30:37 +0900

    Add BlazeFace Model

---
+++ blazeface/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.0'
+__license__ = 'Apache-2.0'
+__author__ = 'tkat0'
+__url__ = 'https://github.com/tkat0/PyTorch_BlazeFace'
+
+from blazeface.model import BlazeFace, BlazeBlock
---
+++ tests/test_blazeface.py
@@ -0,0 +1,20 @@
+import torch
+
+from blazeface import BlazeFace
+
+
+def test_forward():
+    x = torch.randn(1, 3, 128, 128)
+    model = BlazeFace()
+    h = model(x)
+
+    assert h.detach().numpy().shape == (1, 96, 8, 8)
+
+
+def test_export_onnx(tmpdir):
+    p = str(tmpdir.join('blazeface.onnx'))
+
+    x = torch.randn(1, 3, 128, 128)
+    model = BlazeFace()
+    torch.onnx.export(model, x, p, verbose=True, input_names='input',
+                      output_names=['output'])
